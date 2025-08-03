2f5dc5ec3d0faea6972a0cd8298ee2f49f792f2f
Francis Seneve <francisseneve@gmail.com>
2025-08-03 07:14:34 +0100

    Added naming conventions

---
+++ naming/naming_conventions.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func main() {
+	const PI = 3.14
+	fmt.Println("Naming Conventions")
+	fmt.Println(PI)
+}
